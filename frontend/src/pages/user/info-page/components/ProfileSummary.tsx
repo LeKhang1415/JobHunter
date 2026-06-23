74ef9dc5e644a16914369b067b95d6fde7aef59c
khang <chaoskid1415@gmail.com>
2026-06-23 16:29:58 +0700

    feat: scaffold user info page structure and sub-page components

---
+++ frontend/src/pages/user/info-page/components/ProfileSummary.tsx
@@ -0,0 +1,3 @@
+export default function ProfileSummary() {
+  return <div>ProfileSummary</div>;
+}
---
+++ frontend/src/pages/user/info-page/layout/UserInfoLayout.tsx
@@ -0,0 +1,10 @@
+export default function UserInfoLayout() {
+  return (
+    <div>
+      {/* Sidebar will go here */}
+      <div>
+        {/* Outlet will go here */}
+      </div>
+    </div>
+  );
+}
